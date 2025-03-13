8c46b597036469f5ea19dcfe33409c8e809d85e9
Jeonjt <122081342+Jeonjt@users.noreply.github.com>
2025-03-13 15:16:26 +0900

    Create ublox_dual_launch.py

---
+++ ublox_gps/launch/ublox_dual_launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    ublox_gps_launch_dir = get_package_share_directory('ublox_gps')
+    
+    base_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(ublox_gps_launch_dir, 'launch', 'ublox_gps_node_base-launch.py'))
+    )
+    
+    rover_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(ublox_gps_launch_dir, 'launch', 'ublox_gps_node_rover-launch.py'))
+    )
+    
+    return LaunchDescription([
+        base_launch,
+        rover_launch
+    ])
