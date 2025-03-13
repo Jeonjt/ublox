95b840c4926819a2f25955760e6d7b79418c1a54
Jeonjt <122081342+Jeonjt@users.noreply.github.com>
2025-03-13 16:12:09 +0900

    Update ublox_gps_node_dual_launch.py

--- ublox_gps/launch/ublox_gps_node_dual_launch.py
+++ ublox_gps/launch/ublox_gps_node_dual_launch.py
@@ -1,5 +1,6 @@
 from launch import LaunchDescription
-from launch_ros.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription
+
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
 from ament_index_python.packages import get_package_share_directory
